898458e56dd96f660029ee5cfe76276f1fb663ca
imMMX <4isNothing@gmail.com>
2018-10-15 16:08:20 +0800

    Rewrite Dockerfile and entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,70 +1,21 @@
 From buildpack-deps:xenial-scm
-
 LABEL maintainer="MMX <4isnothing@gmail.com>"
 
-
-WORKDIR /usr/local/ss/
-# netease source
-#COPY sources.list /etc/apt/sources.list
-VOLUME /usr/local/ss/
-COPY . /usr/local/ss/
+WORKDIR /opt/ss-mips/
+COPY . .
 RUN apt update && apt install -y \
 	wget \
 	curl \
 	git \
 	autoconf \
 	automake \
+	libtool \
+    gettext  \
+    pkg-config \
 	g++-mipsel-linux-gnu \
 	gcc-mipsel-linux-gnu \
-	gettext build-essential \
-	libtool \
-    gettext
+	g++-mips64-linux-gnuabi64 \
+	gcc-mips64-linux-gnuabi64 \
+	build-essential
 
-# mbedtls
-RUN mbedtls_ver=2.6.0 \
-	&&  wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$mbedtls_ver-gpl.tgz \
-	&& tar xvf mbedtls-$mbedtls_ver-gpl.tgz \
-	&& cd mbedtls-$mbedtls_ver \
-	&& sed -i "s/DESTDIR=\/usr\/local/DESTDIR=\/usr\/local\/mbedtls/g" Makefile \
-	&& CC=mipsel-linux-gnu-gcc AR=mipsel-linux-gnu-ar LD=mipsel-linux-gnu-ld LDFLAGS=-static make \
-	&& make install
-
-# pcre
-RUN pcre_ver=8.41 \
-	&& wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$pcre_ver.tar.gz \
-	&& tar xvf pcre-$pcre_ver.tar.gz \
-	&& cd pcre-$pcre_ver \
-	&& ./configure --host=mipsel-linux-gnu --prefix=/usr/local/pcre --disable-shared --enable-utf8 --enable-unicode-properties \
-	&& make \
-	&& make install
-
-# libsodium
-RUN git clone https://github.com/jedisct1/libsodium \
-	&& cd libsodium/ \
-        && git checkout stable \
-	&& ./autogen.sh \
-	&& ./configure --host=mipsel-linux-gnu --prefix=/usr/local/libsodium --disable-ssp --disable-shared \
-	&& make && make install
-
-# libev
-RUN libev_ver=4.24 \
-	&& wget http://dist.schmorp.de/libev/libev-$libev_ver.tar.gz \
-	&& tar xvf libev-$libev_ver.tar.gz \
-	&& cd libev-$libev_ver \
-	&& ./configure --host=mipsel-linux-gnu --prefix=/usr/local/libev --disable-shared \
-	&& make \
-	&& make install
-
-# c-ares
-RUN cares_ver=1.13.0 \
-	&& wget https://c-ares.haxx.se/download/c-ares-$cares_ver.tar.gz \
-	&& tar xvf c-ares-$cares_ver.tar.gz \
-	&& cd c-ares-$cares_ver \
-	&& ./configure --host=mipsel-linux-gnu --prefix=/usr/local/libcares --disable-shared --enable-utf8 --enable-unicode-properties \
-	&& make \
-	&& make install
-
-# shadowsocks-libev
-RUN chmod +x /usr/local/ss/entrypoint.sh 
-
-ENTRYPOINT ["/usr/local/ss/entrypoint.sh"]
+ENTRYPOINT ["/opt/ss-mips/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,81 @@
 #!/bin/bash
-ver=3.2.0
-cd /usr/local/ss
+
+green="\033[0;32m"
+end="\033[0m"
+# shadowsocks version
+shadowsocks_ver=3.2.0
+# mbedtls
+mbedtls_ver=2.6.0
+# pcre
+pcre_ver=8.41
+# libev
+libev_ver=4.24
+# c-ares
+cares_ver=1.13.0
+# set path
+prefix_path='/opt/ss-mips'
+# mips use mipel-linux-gnu, mips64 use mips64-linux-gnuabi64
+host=mipsel-linux-gnu
+
+echo -e "$green Installing mbedtls...$end"
+wget --no-check-certificate https://tls.mbed.org/download/mbedtls-$mbedtls_ver-gpl.tgz \
+    && tar xvf mbedtls-$mbedtls_ver-gpl.tgz \
+    && cd mbedtls-$mbedtls_ver \
+    && sed -i "s|DESTDIR=/usr/local|DESTDIR=$prefix_path/mbedtls|g" Makefile \
+    && CC=$host-gcc AR=$host-ar LD=$host-ld LDFLAGS=-static make \
+    && make install
+
+echo -e "$green Installing pcre...$end"
+cd
+wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-$pcre_ver.tar.gz \
+    && tar xvf pcre-$pcre_ver.tar.gz \
+    && cd pcre-$pcre_ver \
+    && ./configure --host=$host --prefix=$prefix_path/pcre --disable-shared --enable-utf8 --enable-unicode-properties \
+    && make \
+    && make install
+
+echo -e "$green Installing libsodium...$end"
+cd
+git clone https://github.com/jedisct1/libsodium \
+    && cd libsodium/ \
+        && git checkout stable \
+    && ./autogen.sh \
+    && ./configure --host=$host --prefix=$prefix_path/libsodium --disable-ssp --disable-shared \
+    && make && make install
+
+echo -e "$green Installing libev...$end"
+cd
+wget http://dist.schmorp.de/libev/libev-$libev_ver.tar.gz \
+    && tar xvf libev-$libev_ver.tar.gz \
+    && cd libev-$libev_ver \
+    && ./configure --host=$host --prefix=$prefix_path/libev --disable-shared \
+    && make \
+    && make install
+
+echo -e "$green Installing cares...$end"
+cd
+wget https://c-ares.haxx.se/download/c-ares-$cares_ver.tar.gz \
+    && tar xvf c-ares-$cares_ver.tar.gz \
+    && cd c-ares-$cares_ver \
+    && ./configure --host=$host --prefix=$prefix_path/libcares --disable-shared --enable-utf8 --enable-unicode-properties \
+    && make \
+    && make install
+
+echo -e "$green Installing shadowsocks-libev...$end"
+cd
 git clone https://github.com/shadowsocks/shadowsocks-libev
-cd shadowsocks-libev 
-git checkout v$ver -b v$ver
+cd shadowsocks-libev
+git checkout v$shadowsocks_ver -b v$shadowsocks_ver
 git submodule update --init --recursive && ./autogen.sh
 LIBS="-lpthread -lm" \
-LDFLAGS="-Wl,-static -static -static-libgcc -L/usr/local/libev/lib" \
-CFLAGS="-I/usr/local/libev/include" \
-./configure --host=mipsel-linux-gnu --prefix=/usr/local/ss/erx \
+LDFLAGS="-Wl,-static -static -static-libgcc -L$prefix_path/libev/lib" \
+CFLAGS="-I$prefix_path/libev/include" \
+./configure --host=$host --prefix=$prefix_path/ss-bin \
 --disable-ssp \
 --disable-documentation \
---with-mbedtls=/usr/local/mbedtls \
---with-pcre=/usr/local/pcre \
---with-cares=/usr/local/libcares \
---with-sodium=/usr/local/libsodium \
+--with-mbedtls=$prefix_path/mbedtls \
+--with-pcre=$prefix_path/pcre \
+--with-cares=$prefix_path/libcares \
+--with-sodium=$prefix_path/libsodium \
 && make \
-&& make install \
-&& rm -rf ../shadowsocks-libev
-
-
+&& make install
